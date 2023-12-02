b907a004f8cbb0cfa6717a1e0d156e4989119315
mezray <rmezghani@gmail.com>
2023-12-02 11:28:27 +0100

    Frontend Made by Bdone-hub

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000')
+      .then(response => response.text())
+      .then(data => setMessage(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          {message}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
