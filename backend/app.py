36ef93018c7f391a8925773b3615a9de3d1be7f3
Bdone-hub <67092457+Bdone-hub@users.noreply.github.com>
2023-12-12 15:30:19 +0100

    route pdt que l'autre fout R

--- backend/app.py
+++ backend/app.py
@@ -39,11 +39,73 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 
-@app.route('/')
-def index():
-    cursor.execute("SELECT * FROM mytable")
+@app.route('/add_colis', methods=['POST'])
+def add_colis():
+    data = request.get_json()
+    id = data['id']
+    adresse_x = data['adresse_x']
+    adresse_y = data['adresse_y']
+    cursor = db.cursor()
+
+    # Chercher le numéro de livraison le plus élevé
+    cursor.execute("SELECT MAX(livraison_id) FROM livraisons")
+    livraison_id = cursor.fetchone()[0]
+
+    # Vérifier qu'au maximum 10 colis sont attachés à cette livraison
+    cursor.execute("SELECT COUNT(*) FROM colis WHERE livraison_id = %s", (livraison_id,))
+    count = cursor.fetchone()[0]
+    if count >= 10:
+        # Ajouter une ligne dans la table livraisons avec un id incrémenté
+        cursor.execute("INSERT INTO livraisons (camion_id, etat_livraison) VALUES (%s, %s)", (1, 0))
+        db.commit()
+        livraison_id = cursor.lastrowid
+
+    # Ajouter les informations dans la table colis
+    cursor.execute("INSERT INTO colis (colis_id, livraison_id, adresse_x, adresse_y, etat_colis) VALUES (%s, %s, %s, %s, %s)", (id, livraison_id, adresse_x, adresse_y, 0))
+    db.commit()
+
+    return jsonify({"message": "Colis ajouté avec succès"}), 201
+
+@app.route('/postPosColisFromDevice', methods=['POST'])
+def post_route2():
+    data = request.get_json()
+    colis_id = data.get('colis_id')
+    new_etat_colis = data.get('etat_colis')
+
+    if colis_id and new_etat_colis is not None:
+        cursor = db.cursor()
+        update_query = "UPDATE colis SET etat_colis = %s WHERE colis_id = %s"
+        cursor.execute(update_query, (new_etat_colis, colis_id))
+        db.commit()
+
+    return jsonify(data), 201
+
+@app.route('/postPosCamionFromDevice', methods=['POST'])
+def post_route3():
+    data = request.get_json()
+    camion_id = data.get('camion_id')
+    new_camion_pos_x = data.get('camion_pos_x')
+    new_camion_pos_y = data.get('camion_pos_y')
+
+    if camion_id and new_camion_pos_x is not None and new_camion_pos_y is not None:
+        cursor = db.cursor()
+        update_query = "UPDATE camions SET camion_pos_x = %s, camion_pos_y = %s WHERE camion_id = %s"
+        cursor.execute(update_query, (new_camion_pos_x, new_camion_pos_y, camion_id))
+        db.commit()
+
+    return jsonify(data), 201
+
+@app.route('/getLivraison', methods=['GET'])
+def get_route():
+    query = """
+    SELECT livraisons.livraison_id, colis.colis_id, colis.adresse_x, colis.adresse_y, colis.etat_colis
+    FROM livraisons
+    JOIN colis ON livraisons.livraison_id = colis.livraison_id
+    WHERE livraisons.etat_livraison = 0
+    """
+    cursor.execute(query)
     result = cursor.fetchall()
-    return jsonify(result)
+    return jsonify(result), 201 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
